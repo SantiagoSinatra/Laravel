bb2b0abaad35072c41e32f0be1bd95c6f975799d
SantiagoSinatra <santiagosinatra@gmail.com>
2019-06-05 14:03:07 -0300

    agregado repositorio de laravel con la carpeta de DH

---
+++ Digital House/clase1-laravel/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/ruta1', function () {
+    return "hola mundo";
+});
+
+Route::get('/resultado/{numero}', function ($numero) {
+    if ($numero%2==0){
+        $resultado= 'el numero'. '' . $numero . '' . 'es par';
+    }
+    else{
+       $resultado= 'el numero'. $numero . 'es impar';
+    }
+    return $resultado;
+ });
+ 
+ 
